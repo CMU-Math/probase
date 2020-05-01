144c163d690e3b7b31ba603c44f0d32200144810
Plasma-Vortex <howardhalim@gmail.com>
2020-05-01 01:04:40 -0400

    create comment model

--- database/models.py
+++ database/models.py
@@ -6,26 +6,28 @@ class Problem(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="problems")
     creation_time = models.DateTimeField(auto_now_add=True) 
 
-    problem_text = models.CharField(max_length=5000) # problem (in latex)
+    problem_text = models.CharField(max_length=1000) # problem (in latex)
     answer = models.CharField(max_length=100) # answer (in latex)
     solution = models.CharField(max_length=10000) # solution (in latex)
 
     update_time = models.DateTimeField(null=True) # time of last update
-    update_user = models.ForeignKey(User, null=True, on_delete=models.CASCADE) # user who last updated it
+    update_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="+") # user who last updated it
 
 class QualityRating(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name="qratings")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="qratings")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
     score = models.IntegerField()
 
 class DifficultyRating(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name="dratings")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="dratings")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
     score = models.IntegerField()
 
+class Comment(models.Model):
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    text = models.CharField(max_length=1000) # comment text (in latex)
 
-
-
-
-
-
+    creation_time = models.DateTimeField(auto_now_add=True) 
+    update_time = models.DateTimeField(null=True) # time of last update
+    update_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="+") # user who last updated it
