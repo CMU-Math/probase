bee019fb6a44108e636905767d606c86394d6925
Plasma-Vortex <howardhalim@gmail.com>
2020-05-09 01:07:33 -0400

    Fixed accounts, another migration reset

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    list_display = ('first_name', 'last_name', 'email', 'is_writer', 'is_solver', 'is_staff', 'is_active',)
+    list_filter = ('is_writer', 'is_solver', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('first_name', 'last_name', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('first_name', 'last_name', 'email', 'password1', 'password2')}),
+        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active')}),
+    )
+    search_fields = ('first_name', 'last_name')
+    ordering = ('first_name',)
+
+admin.site.register(User, UserAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -7,24 +7,27 @@ class UserManager(BaseUserManager):
 
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
+    def _create_user(self, email, first_name, last_name, password, **extra_fields):
         if not email:
-            raise ValueError('The given email must be set')
+            raise ValueError('The email must be set')
+        if not first_name:
+            raise ValueError('The first name must be set')
+        if not last_name:
+            raise ValueError('The last name must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
+    def create_user(self, first_name, last_name, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
+    def create_superuser(self, first_name, last_name, email, password, **extra_fields):
+        extra_fields.setdefault('is_writer', True)
+        extra_fields.setdefault('is_solver', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -33,17 +36,20 @@ class UserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
-    is_writer = models.BooleanField('problem writer', default=False)
-    is_solver = models.BooleanField('testsolver', default=False)
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
+    is_writer = models.BooleanField('problem writer status', default=False, help_text='Designates whether the user can submit problems to the database')
+    is_solver = models.BooleanField('testsolver status', default=False, help_text='Designates whether the user can view and rate problems')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     objects = UserManager()
 
-
+    def __str__(self):
+        return self.get_full_name()
