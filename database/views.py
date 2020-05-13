00437d692b24d20c7a453ffeeeae2f65ec0666f0
Plasma-Vortex <howardhalim@gmail.com>
2020-05-13 00:54:34 -0400

    my problems page

--- database/views.py
+++ database/views.py
@@ -7,19 +7,49 @@ from .models import Problem
 from .forms import NewProblemForm, EditProblemForm
 
 def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_solver or request.user.is_staff:
+            return redirect('all_problems')
+        elif request.user.is_writer:
+            return redirect('my_problems')
+        else:
+            staff_list = get_user_model().objects.filter(is_staff=True)
+            return render(request, 'home.html', {'staff_list': staff_list})
+    return render(request, 'home.html')
+
+@login_required
+def all_problems(request):
+    if not request.user.is_solver and not request.user.is_staff:
+        raise PermissionDenied
     problem_list = Problem.objects.all().order_by('-creation_time')
-    return render(request, 'home.html', {'problem_list': problem_list})
+    empty_message = 'There are no problems in the database yet'
+    return render(request, 'problem_list.html', {
+        'problem_list': problem_list,
+        'all_problems': True,
+        'empty_message': empty_message,
+    })
+
+@login_required
+def my_problems(request):
+    if not request.user.is_writer and not request.user.is_staff:
+        raise PermissionDenied
+    problem_list = Problem.objects.filter(author=request.user).order_by('-creation_time')
+    empty_message = "You don't have any problems yet"
+    return render(request, 'problem_list.html', {
+        'problem_list': problem_list,
+        'empty_message': empty_message,
+    })
 
 @login_required
 def problem_detail(request, problem_id):
-    if not request.user.is_writer and not request.user.is_staff:
-        raise PermissionDenied
     problem = get_object_or_404(Problem, pk=problem_id)
+    if not request.user.is_solver and not request.user.is_staff and request.user != problem.author:
+        raise PermissionDenied
     return render(request, 'problem_detail.html', {'problem': problem})
 
 @login_required
 def new_problem(request):
-    if not request.user.is_writer and not request.user.is_staf:
+    if not request.user.is_writer and not request.user.is_staff:
         raise PermissionDenied
     if request.method == 'POST':
         data = {
--- probase/urls.py
+++ probase/urls.py
@@ -1,18 +1,3 @@
-"""probase URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
@@ -22,6 +7,8 @@ from accounts import views as account_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('all/', views.all_problems, name='all_problems'),
+    path('mine/', views.my_problems, name='my_problems'),
     path('new/', views.new_problem, name='new_problem'),
     path('problem/<int:problem_id>/', views.problem_detail, name='problem_detail'),
     path('problem/<int:problem_id>/edit/', views.edit_problem, name='edit_problem'),
