f64aa75e66096e4de0927cf4667dc8c38ad796d4
Plasma-Vortex <howardhalim@gmail.com>
2020-05-27 02:15:05 -0400

    user admin fix default

--- accounts/models.py
+++ accounts/models.py
@@ -21,13 +21,13 @@ class UserManager(BaseUserManager):
     def create_user(self, first_name, last_name, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        extra_fields.setdefault('is_new', True)
         return self._create_user(email, first_name, last_name, password, **extra_fields)
 
     def create_superuser(self, first_name, last_name, email, password, **extra_fields):
         extra_fields.setdefault('is_writer', True)
         extra_fields.setdefault('is_solver', True)
         extra_fields.setdefault('is_new', False)
+        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_staff') is not True:
