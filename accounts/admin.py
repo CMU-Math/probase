19e128bebbebf396e6500761ac2d5342ee2f5b29
Plasma-Vortex <howardhalim@gmail.com>
2020-05-26 21:09:55 -0400

    add is_new to user form

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,11 +10,11 @@ class CustomUserAdmin(UserAdmin):
     list_filter = ('is_writer', 'is_solver', 'is_staff', 'is_active',)
     fieldsets = (
         (None, {'fields': ('first_name', 'last_name', 'email', 'password')}),
-        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active')}),
+        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active', 'is_new')}),
     )
     add_fieldsets = (
         (None, {'fields': ('first_name', 'last_name', 'email', 'password1', 'password2')}),
-        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active')}),
+        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active', 'is_new')}),
     )
     search_fields = ('first_name', 'last_name')
     ordering = ('first_name',)
