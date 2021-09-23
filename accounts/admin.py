48e80a5c15933be1b4470fc899117a6398e247a4
Plasma-Vortex <howardhalim@gmail.com>
2021-09-22 22:36:07 -0400

    Admin action for setting users active/inactive

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,20 +4,80 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
+@admin.action(description='Make selected users active')
+def make_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+@admin.action(description='Make selected users inactive')
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('first_name', 'last_name', 'email', 'is_writer', 'is_solver', 'is_staff', 'is_active',)
-    list_filter = ('is_writer', 'is_solver', 'is_staff', 'is_active',)
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'is_writer',
+        'is_solver',
+        'is_staff',
+        'is_active',
+    )
+    list_filter = (
+        'is_writer',
+        'is_solver',
+        'is_staff',
+        'is_active',
+    )
     fieldsets = (
-        (None, {'fields': ('first_name', 'last_name', 'email', 'password')}),
-        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active', 'is_new')}),
+        (None, {
+            'fields': (
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+            )
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_writer',
+                'is_solver',
+                'is_staff',
+                'is_active',
+                'is_new',
+            )
+        }),
     )
     add_fieldsets = (
-        (None, {'fields': ('first_name', 'last_name', 'email', 'password1', 'password2')}),
-        ('Permissions', {'fields': ('is_writer', 'is_solver', 'is_staff', 'is_active', 'is_new')}),
+        (None, {
+            'fields': (
+                'first_name',
+                'last_name',
+                'email',
+                'password1',
+                'password2'
+            )
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_writer',
+                'is_solver',
+                'is_staff',
+                'is_active',
+                'is_new',
+            )
+        }),
     )
-    search_fields = ('first_name', 'last_name')
+    search_fields = (
+        'first_name',
+        'last_name',
+    )
     ordering = ('pk',)
+    actions = (
+        make_active,
+        make_inactive,
+    )
 
 admin.site.register(User, CustomUserAdmin)
 
--- database/admin.py
+++ database/admin.py
@@ -10,10 +10,29 @@ def unarchive(modeladmin, request, queryset):
     queryset.update(is_archived=False)
 
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'subject', 'is_archived')
-    search_fields = ('id', 'title', 'author', 'problem_text', 'answer', 'solution')
-    list_filter = ('subject', 'is_archived')
-    actions = [archive, unarchive]
+    list_display = (
+        'id',
+        'title',
+        'author',
+        'subject',
+        'is_archived',
+    )
+    search_fields = (
+        'id',
+        'title',
+        'author',
+        'problem_text', 
+        'answer',
+        'solution',
+    )
+    list_filter = (
+        'subject',
+        'is_archived',
+    )
+    actions = (
+        archive,
+        unarchive,
+    )
 
 admin.site.register(Problem, ProblemAdmin)
 admin.site.register(Rating)
