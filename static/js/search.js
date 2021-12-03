54b28ee9c91620d47cd0b7f858cf952eb6223646
Robert Chen <robertchen@live.com>
2021-12-03 16:35:27 -0500

    feat: add searching

---
+++ static/js/search.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+	let textFilter = "";
+	let authorFilter = [];
+
+	const getProblems = () => Array.from($(".card[data-type=problem]"));
+
+
+	const authorElem = $("#filterAuthor")[0];
+	const applyFilter = () => {
+		const matches = getProblems()
+			.filter(e => {
+				if (textFilter !== "") {
+					return e.innerText.includes(textFilter);
+				}
+				return true;
+			})
+			.filter(e => {
+				if (authorFilter.length !== 0) {
+					return authorFilter.includes(e.dataset.author);
+				}
+				return true;
+			});
+
+		getProblems().forEach(e => {
+			e.hidden = !matches.includes(e);
+		});
+	}
+	$("#filterText")[0].oninput = function () {
+		textFilter = this.value;
+		applyFilter();
+	}
+
+	getProblems().map(e => e.dataset.author)
+		.filter((e, i, arr) => arr.indexOf(e) === i)
+		.concat(["awer", "woeijr"])
+		.forEach(author => {
+			const opt = document.createElement("option");
+			opt.value = author;
+			opt.innerText = author;
+			authorElem.appendChild(opt);
+		});
+
+	authorElem.onchange = function () {
+		authorFilter = Array.from(this.children)
+			.filter(a => a.selected)
+			.filter(a => a.value !== "")
+			.map(a => a.value);
+		applyFilter();
+	}
+
+}
